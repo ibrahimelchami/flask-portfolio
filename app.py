284b3e92cff6aca25bba890747e67a29309b7ca0
Ibrahim El Chami <ibelchami@gmail.com>
2021-01-02 13:05:36 +0000

    Minor adjustments to links

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from collections import OrderedDict
 import ast
 
@@ -12,17 +12,22 @@ def index():
 	#print (projects)
 	return render_template('index.html', projects=projects)
     
-@app.route('/projects/<project>')
-def projects(project):
+@app.route('/projects/<project_url>')
+def projects(project_url):
 	with open('static/projects.txt') as f:
 		projects = f.read()
 	projects = ast.literal_eval(projects)
+	print(project_url)
+	
+	for item in projects:
+		print(item['heading'])
+		if project_url == item['heading'].lower():
+			project = item
+			print(project)
+			return render_template('projects.html', project=project)
 
-	for project in projects:
-		if project == project['heading']:
-			project = project
-
-	return render_template('projects.html', project=project)
+	#catch bad URL and redirect home
+	return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
